57e588a0152b66ff9eb801f47f56a9f90f08bbba
Coding Buddy <nomail@nomail.com>
2021-06-19 00:00:42 +0300

    Changes to allow desktop build. Upgrade to lwjgl3. Change "setWindowSizeLimits" based on updated API.

--- java/desktop/src/com/ygames/ysoccer/desktop/DesktopLauncher.java
+++ java/desktop/src/com/ygames/ysoccer/desktop/DesktopLauncher.java
@@ -1,18 +1,16 @@
 package com.ygames.ysoccer.desktop;
 
 import com.badlogic.gdx.Files;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.ygames.ysoccer.YSoccer;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = 1280;
-        config.height = 720;
-        config.forceExit = false;
-        config.addIcon("images/icon_128.png", Files.FileType.Internal);
-        config.addIcon("images/icon_32.png", Files.FileType.Internal);
-        new LwjglApplication(new YSoccer(), config);
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setWindowSizeLimits(1280, 720, 9999, 9999);
+        config.setWindowIcon(Files.FileType.Internal, "images/icon_128.png");
+        config.setWindowIcon(Files.FileType.Internal, "images/icon_32.png");
+        new Lwjgl3Application(new YSoccer(), config);
     }
 }
